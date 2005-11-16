d306a08701673d14f49cbf01d3f02f448503b463
Manu Evans <turkeyman@gmail.com>
2005-11-16 14:26:12 +0000

    oops
    git-svn-id: https://fuji.googlecode.com/svn/trunk@215 7e75b669-355e-e21c-2746-f45317fb21cf

---
+++ Fuji/Private/Source/MFSound_Internal.h
@@ -0,0 +1,12 @@
+#if !defined(_MFSOUND_INTERNAL_H)
+#define _MFSOUND_INTERNAL_H
+
+#include "MFSound.h"
+
+void MFSound_InitModule();
+void MFSound_DeinitModule();
+
+void MFSound_Update();
+void MFSound_Draw();
+
+#endif
