3b0f28d562e95c56057ba078043cb8f67ff7b089
Manu Evans <turkeyman@gmail.com>
2006-04-17 09:18:15 +0000

    - Forgot a file.
    git-svn-id: https://fuji.googlecode.com/svn/trunk@290 7e75b669-355e-e21c-2746-f45317fb21cf

---
+++ Fuji/Private/PSP/MFTranslation_PSP.cpp
@@ -0,0 +1,53 @@
+#include "Fuji.h"
+#include "MFTranslation.h"
+
+#include <psputility_sysparam.h>
+
+MFLanguage MFTranslation_GetDefaultLanguage()
+{
+	MFLanguage language;
+
+	int lang;
+
+	int r = sceUtilityGetSystemParamInt(PSP_SYSTEMPARAM_ID_INT_LANGUAGE, &lang);
+	MFDebug_Assert(r == 0, "sceUtilityGetSystemParamInt Failed..");
+
+	switch(lang)
+	{
+		case PSP_SYSTEMPARAM_LANGUAGE_ENGLISH:
+			language = MFLang_English;
+			break;
+		case PSP_SYSTEMPARAM_LANGUAGE_FRENCH:
+			language = MFLang_French;
+			break;
+		case PSP_SYSTEMPARAM_LANGUAGE_SPANISH:
+			language = MFLang_Spanish;
+			break;
+		case PSP_SYSTEMPARAM_LANGUAGE_GERMAN:
+			language = MFLang_German;
+			break;
+		case PSP_SYSTEMPARAM_LANGUAGE_ITALIAN:
+			language = MFLang_Italian;
+			break;
+
+		case PSP_SYSTEMPARAM_LANGUAGE_JAPANESE:
+			language = MFLang_Japanese;
+			break;
+		case PSP_SYSTEMPARAM_LANGUAGE_KOREAN:
+			language = MFLang_Korean;
+			break;
+
+		case PSP_SYSTEMPARAM_LANGUAGE_DUTCH:
+			language = MFLang_German;
+			break;
+		case PSP_SYSTEMPARAM_LANGUAGE_PORTUGUESE:
+			language = MFLang_Portuguese;
+			break;
+
+		default:
+			language = MFLang_English;
+			break;
+	}
+
+	return language;
+}
