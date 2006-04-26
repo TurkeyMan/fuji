b0c67472a25f25b4cce60da03359822fda639fbe
Manu Evans <turkeyman@gmail.com>
2006-04-26 14:24:28 +0000

    - Linux changes
    git-svn-id: https://fuji.googlecode.com/svn/trunk@295 7e75b669-355e-e21c-2746-f45317fb21cf

--- Fuji/Private/Linux/Display_Linux.cpp
+++ Fuji/Private/Linux/Display_Linux.cpp
@@ -3,35 +3,45 @@
 
 void MFDisplay_DestroyWindow()
 {
+	// need to destroy the window here
 }
 
 int MFDisplay_CreateDisplay(int width, int height, int bpp, int rate, bool vsync, bool triplebuffer, bool wide, bool progressive)
 {
+	// need to create a window and the opngl context here
+
 	return 0;
 }
 
 void MFDisplay_ResetDisplay()
 {
+	// probably dont need to do anything here
 }
 
 void MFDisplay_DestroyDisplay()
 {
+	// need to destroy the opengl contxt here
 }
 
 void MFDisplay_BeginFrame()
 {
+	// begin the frame
 }
 
 void MFDisplay_EndFrame()
 {
+	// end the frame and swap buffers
 }
 
 void MFDisplay_SetClearColour(float r, float g, float b, float a)
 {
+	glClearColor(r, g, b, a);
 }
 
 void MFDisplay_ClearScreen(uint32 flags)
 {
+	int mask = ((flags & CS_Colour) ? GL_COLOR_BUFFER_BIT : 0) | ((flags & CS_ZBuffer) ? GL_DEPTH_BUFFER_BIT : 0);
+	glClear(mask);
 }
 
 void MFDisplay_SetViewport(float x, float y, float width, float height)
--- Fuji/Private/Linux/MFPrimitive_Linux.cpp
+++ Fuji/Private/Linux/MFPrimitive_Linux.cpp
@@ -4,6 +4,19 @@
 uint32 beginCount;
 uint32 currentVert;
 
+uint32 primType;
+
+static int gPrimTypes[7] =
+{
+	GL_POINTS,
+	GL_LINES,
+	GL_LINE_STRIP,
+	GL_TRIANGLES,
+	GL_TRIANGLE_STRIP,
+	GL_TRIANGLE_FAN,
+	GL_QUADS
+};
+
 void MFPrimitive_InitModule()
 {
 	MFCALLSTACK;
@@ -20,11 +33,14 @@ void MFPrimitive_DrawStats()
 
 void MFPrimitive(uint32 type, uint32 hint)
 {
+	primType = type & PT_PrimMask;
 }
 
 void MFBegin(uint32 vertexCount)
 {
 	beginCount = vertexCount;
+
+	glBegin(gPrimTypes[primType]);
 }
 
 void MFSetMatrix(const MFMatrix &mat)
@@ -33,39 +49,54 @@ void MFSetMatrix(const MFMatrix &mat)
 
 void MFSetColour(const MFVector &colour)
 {
+	glColor4f(colour.x, colour.y, colour.z, colour.w);
 }
 
 void MFSetColour(float r, float g, float b, float a)
 {
+	glColor4f(r, g, b, a);
 }
 
 void MFSetColour(uint32 col)
 {
+	// TODO: work out raw colour byte order
 }
 
 void MFSetTexCoord1(float u, float v)
 {
+	glTexCoord2f(u, v);
 }
 
 void MFSetNormal(const MFVector &normal)
 {
+	glNormal3f(normal.x, normal.y, normal.z);
 }
 
 void MFSetNormal(float x, float y, float z)
 {
+	glNormal3f(x, y, z);
 }
 
 void MFSetPosition(const MFVector &pos)
 {
-	++currentVert;
+	MFSetPosition(pos.x, pos.y, pos.z);
 }
 
 void MFSetPosition(float x, float y, float z)
 {
+	glVertex3f(x, y, z);
+
+	if(primType == PT_QuadList && (currentVert & 1))
+	{
+		// TODO: add top right and bottom lft verts
+	}
+
 	++currentVert;
 }
 
 void MFEnd()
 {
 	MFDebug_Assert(currentVert == beginCount, "Incorrect number of vertices.");
+
+	glEnd();
 }
