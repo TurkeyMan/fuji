2380e4f287dca95460c43b3d3e20bd25b2daa9e4
Manu Evans <turkeyman@gmail.com>
2011-03-13 09:00:47 +0000

    - Twiddled the colour channels, fixes D3D9 renderer ;)
    git-svn-id: https://fuji.googlecode.com/svn/trunk@743 7e75b669-355e-e21c-2746-f45317fb21cf

--- Fuji/Private/Source/Asset/MFIntTexture.cpp
+++ Fuji/Private/Source/Asset/MFIntTexture.cpp
@@ -1419,9 +1419,9 @@ MFTextureFormat ChooseBestFormat(MFIntTexture *pTexture, MFPlatform platform)
 				targetFormat = TexFmt_DXT5;
 */
 			if(pTexture->opaque)
-				targetFormat = TexFmt_A8B8G8R8; //TexFmt_R5G6B5;
+				targetFormat = TexFmt_A8R8G8B8; //TexFmt_R5G6B5;
 			else if(pTexture->oneBitAlpha)
-				targetFormat = TexFmt_A1R5G5B5;
+				targetFormat = TexFmt_A8R8G8B8; //TexFmt_A1R5G5B5;
 			else
 				targetFormat = TexFmt_A8R8G8B8;
 			break;
