826a43f15359f28d16a137cc7b02c2c12498c57b
Manu Evans <turkeyman@gmail.com>
2014-01-22 12:23:03 +1000

    Updated for premake5.

--- dist/Samples/create_project.bat
+++ dist/Samples/create_project.bat
@@ -1,18 +1,16 @@
 @ECHO OFF
 
 ECHO Select the type of project you would like to create:
-ECHO 1. Visual Studio 2010 Solution
-ECHO 2. Visual Studio 2008 Solution
-ECHO 3. Visual Studio 2005 Solution
-ECHO 4. Visual Studio 2003 Solution
-ECHO 5. Visual Studio 2002 Solution
-ECHO 6. Code::Blocks Project
-ECHO 7. CodeLite Project
-ECHO 8. GNU Makefile
+ECHO 1. Visual Studio 2012 Solution
+ECHO 2. Visual Studio 2010 Solution
+ECHO 3. Visual Studio 2008 Solution
+ECHO 4. Visual Studio 2005 Solution
+ECHO 5. Visual Studio 2003 Solution
+ECHO 6. Visual Studio 2002 Solution
+ECHO 7. GNU Makefile
 
-CHOICE /N /C:12345678 /M "[1-8]:"
+CHOICE /N /C:1234567 /M "[1-7]:"
 
-IF ERRORLEVEL ==8 GOTO EIGHT
 IF ERRORLEVEL ==7 GOTO SEVEN
 IF ERRORLEVEL ==6 GOTO SIX
 IF ERRORLEVEL ==5 GOTO FIVE
@@ -22,37 +20,33 @@ IF ERRORLEVEL ==2 GOTO TWO
 IF ERRORLEVEL ==1 GOTO ONE
 GOTO END
 
-:EIGHT
- ECHO Creating GNU Makefile...
- ..\bin\premake4.exe gmake
- GOTO END
 :SEVEN
- ECHO Creating CodeLite Project...
- ..\bin\premake4.exe codelite
+ ECHO Creating GNU Makefile...
+ ..\bin\premake5.exe gmake
  GOTO END
 :SIX
- ECHO Creating Code::Blocks Project...
- ..\bin\premake4.exe codeblocks
+ ECHO Creating VS2002 Project...
+ ..\bin\premake5.exe vs2002
  GOTO END
 :FIVE
- ECHO Creating VS2002 Project...
- ..\bin\premake4.exe vs2002
+ ECHO Creating VS2003 Project...
+ ..\bin\premake5.exe vs2003
  GOTO END
 :FOUR
- ECHO Creating VS2003 Project...
- ..\bin\premake4.exe vs2003
+ ECHO Creating VS2005 Project...
+ ..\bin\premake5.exe vs2005
  GOTO END
 :THREE
- ECHO Creating VS2005 Project...
- ..\bin\premake4.exe vs2005
+ ECHO Creating VS2008 Project...
+ ..\bin\premake5.exe vs2008
  GOTO END
 :TWO
- ECHO Creating VS2008 Project...
- ..\bin\premake4.exe vs2008
+ ECHO Creating VS2010 Project...
+ ..\bin\premake5.exe vs2010
  GOTO END
 :ONE
- ECHO Creating VS2010 Project...
- ..\bin\premake4.exe vs2010
+ ECHO Creating VS2012 Project...
+ ..\bin\premake5.exe vs2012
  GOTO END
 
 :END
