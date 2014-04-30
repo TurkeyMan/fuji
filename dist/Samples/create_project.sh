0d7ad5e6df8bd84ce380c22034cd7b9e3c4edc3b
Manu Evans <turkeyman@gmail.com>
2014-05-01 00:15:52 +1000

    Fixed Samples create_project.sh

--- dist/Samples/create_project.sh
+++ dist/Samples/create_project.sh
@@ -1,1 +1,17 @@
-premake4 gmake
+echo "Select the type of project you would like to create:"
+echo "1. GNU Makefile"
+echo "2. MonoDevelop (Mono-D)"
+read -p "[1-2] " -n 1 -r
+echo
+if [[ $REPLY =~ "1" ]]
+then
+	echo "Creating GNU Makefile..."
+	../bin/premake5 gmake
+elif [[ $REPLY =~ "2" ]]
+then
+	echo "Creating MonoDevelop project..."
+	../bin/premake5 monodevelop
+else
+	echo "Invalid input: "$REPLY
+fi
+
